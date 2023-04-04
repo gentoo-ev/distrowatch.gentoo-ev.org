22b3162c73a3723cd884ac48ed1f47de48655521
Sebastian Pipping <sebastian@pipping.org>
2023-04-05 00:12:22 +0200

    entrypoint.bash: Change directory first

--- entrypoint.bash
+++ entrypoint.bash
@@ -2,14 +2,13 @@
 # Copyright (C) 2018 Sebastian Pipping <sebastian@pipping.org>
 # Licensed under the MIT license
 
-if [[ $# -gt 0 ]]; then
-    exec "$@"
-fi
-
-
 set -e
 
 cd /var/www/distrowatch-gentoo-ev-org/
+
+if [[ $# -gt 0 ]]; then
+    exec "$@"
+fi
 
 while true; do
     if emerge-webrsync; then
